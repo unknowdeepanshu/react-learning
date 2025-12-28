ae1146b566fa378f3a97c0cddf17d4244b86e641
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2025-12-28 17:25:00 +0530

    first commit

---
+++ start_over_again/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Learning from chai aur code </h1>
+    <p>
+      This is my first React app after learning from Chai aur code YouTube channel.
+      I am so excited to learn React and build amazing projects with it.
+    </p>
+    <h2> What I learned today</h2>
+    <p>
+      
+      Today 12/27/2025,
+      I learned is not best in seo for react apps.
+      But I will learn more about it in future.
+      <ol style={{textAlign:'-webkit-left'}}>
+        <li>  also learn about vite + react setup 
+        </li>
+        <li> learned about React in vite each component is in jsx file.
+        </li>
+        <li> learned about React each component structure call function in upper case first letter and return jsx code(html code in js file) 
+        </li>
+        <li>  learned about React how to import css file in react component file. 
+        </li>
+        <li> learned about React how to export component from one file and import in another file.  
+        </li>
+      </ol>
+       
+    </p>
+
+    </>
+  )
+}
+
+export default App
