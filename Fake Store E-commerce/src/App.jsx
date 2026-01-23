7759275c88de0a255d9a6abd7614ef3a02e1d610
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-01-23 12:23:05 +0530

    add all configure for tailwind , prettierrc and redux

---
+++ Fake Store E-commerce/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>I am dipanshu starting</h1>
+      <h2>Building Fake Store E-commerce</h2>
+    </>
+  );
+}
+
+export default App;
